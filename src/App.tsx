c7a758262c187d3d1d5a86ddd7e2bfef7ef3122e
Serzh108 <136076826+Serzh108Tetra@users.noreply.github.com>
2025-04-28 13:50:31 +0300

    EDIT | start of task

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import logo from './assets/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={logo} className="logo" alt="logo" />
+        </a>
+      </div>
+      <h1>Date + Time peackers</h1>
+
+      <p className="read-the-docs">Click on SerZH logo to learn React more</p>
+    </>
+  );
+}
+
+export default App;
